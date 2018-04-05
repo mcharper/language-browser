fa17615d5e29a12ca6ed939942b58861dd7efc8f
Mike Harper <mike@mcharper.com>
2018-04-05 16:29:31 +0100

    Prototype the app

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import Dracula from 'graphdracula';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+          var Graph = Dracula.Graph
+          var Renderer = Dracula.Renderer.Raphael
+          var Layout = Dracula.Layout.Spring
+          
+          var graph = new Graph()
+
+          graph.addEdge('Paradigm', 'Imperative', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Has similar syntax to'
+                                      }})
+          graph.addEdge('Paradigm', 'Procedural', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Has similar syntax to'
+                                      }})
+          graph.addEdge('Paradigm', 'Object Oriented', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Has similar syntax to'
+                                      }})
+          graph.addEdge('Paradigm', 'Functional', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Has similar syntax to'
+                                      }})
+
+          graph.addEdge('C', 'Procedural', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Supports paradigm'
+                                      }})
+
+          graph.addEdge('C++', 'C', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Has similar syntax to'
+                                      }})
+          graph.addEdge('C++', 'Object Oriented', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Supports paradigm'
+                                      }})
+                                      
+          graph.addEdge('C#', 'C', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Has similar syntax to'
+                                      }})
+
+          graph.addEdge('C#', 'Object Oriented', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Supports paradigm'
+                                      }})
+
+          graph.addEdge('JavaScript', 'C', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Has similar syntax to'
+                                      }})
+
+          graph.addEdge('JavaScript', 'Functional', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Supports paradigm'
+                                      }})
+
+          graph.addEdge('JavaScript', 'Object Oriented', { directed : true,
+                                      style: {
+                                        stroke: '#ddd',
+                                        fill: '#ddd',
+                                        label: 'Supports paradigm'
+                                      }})
+                                      
+          var layout = new Layout(graph)
+          var renderer = new Renderer('#paper', graph, 800, 600)
+          renderer.draw()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div id="paper">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
